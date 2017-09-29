650b934c57b7aab4a65cc5156ad99b550ce98cca
Mike Frank <mike@bluefrog.ca>
2017-09-29 11:53:19 -0500

    Removed aws deployment details from gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,5 @@
 var gulp = require('gulp'),
-    awspublish = require('gulp-awspublish'),
-    cloudfront = require('gulp-cloudfront-invalidate-aws-publish')
-    aws = require('aws-sdk'),
     bower = require('gulp-bower'),
-    parallelize = require("concurrent-transform"),
     less = require('gulp-less'),
     cssmin = require('gulp-cssmin'),
     concat = require('gulp-concat'),
@@ -22,31 +18,4 @@ gulp.task('bower', function () {
     return bower();
 });
 
-gulp.task('publish', function() {
-    bower();
-
-    // var awsCredentials =  new aws.SharedIniFileCredentials({
-    //     profile: 'bluefrog'
-    // });
-
-    // var publisher = awspublish.create({
-    //     region: 'us-east-1',
-    //     credentials: awsCredentials,
-    //     params: {
-    //         Bucket: 'bluefrog-ca-website'
-    //     }
-    // });
-
-    // return gulp.src(['**', '!node_modules/**'])
-    //     .pipe(parallelize(publisher.publish(), 10))
-    //     .pipe(cloudfront({
-    //         distribution: 'E2107SD7GS1DUD',
-    //         indexRootPath: true,
-    //         credentials: awsCredentials
-    //     }))
-    //     .pipe(publisher.sync())
-    //     .pipe(publisher.cache())
-    //     .pipe(awspublish.reporter());
-});
-
-gulp.task('default', ['bower', 'build-css', 'publish']);
+gulp.task('default', ['bower', 'build-css']);
